f24bcdf5473574bda94b3d013c16ecceb608832e
Matthew Ma <mtthman@amazon.com>
2021-04-30 21:51:40 -0700

    Integrate Google Maps API

--- hospital-locator/src/App.js
+++ hospital-locator/src/App.js
@@ -2,21 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var origin = '23.7248,75.9921'
+  var destination = '23.7185370,76.0004910'
+  fetch(`http://localhost:3001/maps/${origin}/${destination}`)
+  .then(res => res.text())
+  .then(console.log)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        BOILERPLATE
       </header>
     </div>
   );
