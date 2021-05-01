b39659fc9fcf5c1d4bb8a4eaf4b2fb5b81c1ef66
Kristin Anthony <anthkris@MacBook-Pro.local>
2021-05-01 00:24:36 -0700

    Add fake data matching data scrape - Update HospitalList to respond to more realistic data shape - Improve icon a11y - TODO: Need to sort data (or receive sorted data) and calculate distance of hospital from user

--- hospital-locator/src/hospitalList/HospitalList.js
+++ hospital-locator/src/hospitalList/HospitalList.js
@@ -1,26 +1,42 @@
 import "./HospitalList.css";
+import testData from "../__mocks__/testData.json";
 import { FaBed } from "react-icons/fa";
 
-const HospitalList = ({ hospital }) => {
+const HospitalList = ({ hospitals }) => {
   // Faking hospital data to show list
-  const hospitals = Array.from(Array(5)).map((_, i) => {
+  // Would probably be more performant to recieve sorted data from the backend?
+  const hospitalArr = testData.map((hospital, i) => {
     console.log(i);
     return <HospitalListItem key={`hospital-${i}`} hospital={hospital} />;
   });
 
-  return <ul className="hospitalList">{hospitals}</ul>;
+  return <ul className="hospitalList">{hospitalArr}</ul>;
 };
 
 const HospitalListItem = ({ hospital }) => {
+  /* From data scrape, expect hospital to contain
+    name, lat, lon, type, total_beds, available_beds, rates_info, contact(arr)
+  */
+  const contactArr = hospital.contact.map((contactMethod, i, hospital) => {
+    return (
+      <p key={`${hospital.name}-contactMethod-${i}`}>
+        Phone: {contactMethod.phone}
+      </p>
+    );
+  });
   return (
     <li className="hospitalListItem">
       <div className="hospitalInfo">
-        <h2>Hospital Name</h2>
-        <p>Distance From Hospital</p>
+        <h2>{hospital.name}</h2>
+        <p>Distance From user</p>
+        {contactArr}
       </div>
       <div className="hospitalBeds">
-        <FaBed className="hospitalBedIcon" title="Number of hospital beds" />
-        <p>Num Beds</p>
+        <FaBed
+          className="hospitalBedIcon"
+          aria-labelledby="Number of available hospital beds"
+        />
+        <p>{hospital["available_beds"]}</p>
       </div>
     </li>
   );
