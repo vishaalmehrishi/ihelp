ef40aceda6ddf93651178b8e158b447eab193f85
Utsav Popli <popliu@amazon.com>
2021-05-01 20:06:21 -0700

    Task 03486- Add header and fix little bit of layout

--- hospital-locator/src/HomePage/HomePage.js
+++ hospital-locator/src/HomePage/HomePage.js
@@ -1,14 +1,15 @@
 import SearchBar from "../SearchBar/SearchBar";
 import "./HomePage.css";
 import "../SearchBar/SearchBar";
+import { Header } from "../Header/Header";
 import { HospitalList } from "../HospitalList/HospitalList";
-import { LocationWidget } from "../Location/LocationWidget";
+
 
 function HomePage() {
   return (
     <div className="homepage-root">
+      <Header /> 
       <SearchBar />
-      <LocationWidget />
       <HospitalList />
     </div>
   );
