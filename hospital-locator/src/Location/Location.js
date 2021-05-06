875937aee02d1997fe57e456e735ed93a4688ec1
Kristin Anthony <anthkris@MacBook-Pro.local>
2021-05-05 18:30:24 -0700

    Move api keys to .env

--- hospital-locator/src/Location/Location.js
+++ hospital-locator/src/Location/Location.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { RiContactsBookLine } from "react-icons/ri";
-const GOOGLE_API_KEY = require("../apiKeys");
+
+const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
 const GEOCODE_API = "https://maps.googleapis.com/maps/api/geocode/json?";
-const PLACES_API = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?";
+const PLACES_API =
+  "https://maps.googleapis.com/maps/api/place/nearbysearch/json?";
 const PLACES_RADIUS = 50;
 
 const getCityFromCoordinates = (coordinates) => {
@@ -87,29 +88,31 @@ const useLocation = (
 //getNearByHospitalsFromLocation(`28.704060,77.102493`)
 //getNearByHospitalsFromLocation(`${location.latitude},${location.longitude}`)
 const getNearByHospitalsFromLocation = (coordinates) => {
-    return new Promise((resolve, reject) => {
-        const api = `${PLACES_API}location=${coordinates}&radius=${PLACES_RADIUS}&type=hospital&key=${GOOGLE_API_KEY}`
-        console.log(api)
-         fetch(api,{
-             mode: "no-cors",
-            headers: {
-                'content-type': 'application/json',
-            }
-         }).then((res) => {
-            //console.log(res) 
-            return res.json()
-         }).then (res => { 
-                if (res.status == "OK") {
-                    console.log(res.json.results)
-                    resolve(res.results)
-                } else {
-                    reject("getNearByHospitalsFromLocation failed")
-                }
-        }).catch(reject)
-        
-    }).catch(msg => {
-        console.log("getNearByHospitalsFromLocation exception ", msg)
+  return new Promise((resolve, reject) => {
+    const api = `${PLACES_API}location=${coordinates}&radius=${PLACES_RADIUS}&type=hospital&key=${GOOGLE_API_KEY}`;
+    console.log(api);
+    fetch(api, {
+      mode: "no-cors",
+      headers: {
+        "content-type": "application/json"
+      }
     })
+      .then((res) => {
+        //console.log(res)
+        return res.json();
+      })
+      .then((res) => {
+        if (res.status == "OK") {
+          console.log(res.json.results);
+          resolve(res.results);
+        } else {
+          reject("getNearByHospitalsFromLocation failed");
+        }
+      })
+      .catch(reject);
+  }).catch((msg) => {
+    console.log("getNearByHospitalsFromLocation exception ", msg);
+  });
 };
 
 export {
